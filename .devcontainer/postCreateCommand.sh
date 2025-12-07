9bfc31950b9043d66671ef85dcb890cb83f034c0
Geoff Stockham <geoff@stormid.com>
2025-12-07 17:40:32 +0000

    Commit remaining workspace changes

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,8 @@
 
 npm install -g @openai/codex
 npm install -g @google/gemini-cli
+npm install -g @fission-ai/openspec@latest
+
+codex mcp add context7 --url  "https://mcp.context7.com/mcp"
+codex mcp add mslearn --url  "https://learn.microsoft.com/api/mcp"
+codex mcp add playwright npx "@playwright/mcp@latest"
